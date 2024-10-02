cd0054bc3be535c1a44604606196de4bc0e5b83a
Blxssy <roma.asav@gmail.com>
2024-10-02 11:31:45 +0300

    added docker photo-service

--- backend/photo-service/cmd/main.go
+++ backend/photo-service/cmd/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"github.com/Blxssy/social-media/photo-service/intenal/app"
 	"github.com/Blxssy/social-media/photo-service/intenal/config"
 	"github.com/Blxssy/social-media/photo-service/intenal/storage"
 	"github.com/Blxssy/social-media/photo-service/pkg/logger"
 	"log/slog"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
@@ -15,4 +19,17 @@ func main() {
 
 	store := storage.NewStorage(cfg, logger)
 
+	application := app.New(logger, cfg.GRPC.Port, store)
+
+	go func() {
+		application.GRPCServer.MustRun()
+	}()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, syscall.SIGINT, syscall.SIGTERM)
+
+	<-stop
+
+	application.GRPCServer.Stop()
+	logger.Info("Server stopped")
 }
